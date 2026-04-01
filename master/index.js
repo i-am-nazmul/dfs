914dc94fb3b1e619c0cba68b98f884065b50f2f9
Nazmul <mdnazmul14429@gmail.com>
2026-04-02 01:06:18 +0530

    Initialise the master server

---
+++ master/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello from Master');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Master running on http://localhost:${PORT}`);
+});
