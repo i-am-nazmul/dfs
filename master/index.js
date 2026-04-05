61ea576b4251c5f3c5c5979b97bd74b6876481b9
Nazmul <mdnazmul14429@gmail.com>
2026-04-05 17:54:47 +0530

    Establish connection from master

--- master/index.js
+++ master/index.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Master running on http://localhost:${PORT}`);
+  console.log(`Master running on PORT : ${PORT}`);
 });
--- worker1/index.js
+++ worker1/index.js
@@ -3,12 +3,29 @@ import express from 'express';
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
 // Basic route
 app.get('/', (req, res) => {
   res.send('Hello from Worker1');
 });
 
+// ✅ Test route (for master communication)
+app.get('/test', (req, res) => {
+  res.send('Worker1 is working properly!');
+});
+
+// ✅ Future: store file chunk
+app.post('/store', (req, res) => {
+  const { chunkName, data } = req.body;
+
+  console.log(`Received chunk: ${chunkName}`);
+
+  // (Later we will save this to file system)
+  res.send('Chunk received successfully');
+});
+
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Worker1 running on PORT : ${PORT}`);
 });
