88a1ac14c24d2bb958fa5edef82eae13ea1a8497
Nazmul <mdnazmul14429@gmail.com>
2026-04-12 16:03:10 +0530

    Add logger in master node

--- master/index.js
+++ master/index.js
@@ -4,7 +4,7 @@ import authRoutes from './routes/authRoutes.js';
 import fileRoutes from './routes/fileRoutes.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(express.json());
 
@@ -17,6 +17,18 @@ app.use('/auth', authRoutes);
 app.use('/files', fileRoutes);
 
 // Start server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Master running on PORT : ${PORT}`);
+});
+
+server.on('error', (error) => {
+  console.error('Server failed to start:', error);
+});
+
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught exception:', error);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled rejection:', reason);
 });
